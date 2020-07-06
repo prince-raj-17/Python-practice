0a4e92acd6c7a1c6b717a3f280826d3dca16865b
prince-raj-17 <raj1999prince@gmail.com>
2020-07-06 16:43:22 +0530

    Adding two numbers

---
+++ sum.py
@@ -0,0 +1,17 @@
+# This program add two numbers.
+
+num1 = 2
+num2 = 3
+
+sum = num1+num2
+
+print("The sum of {} and {} is {}".format(num1,num2,sum))
+
+# We can use input method to take number from user and add them.
+
+num1 = input("Enter your first number here. ")
+num2 = input("Enter your second number here. ")
+
+sum = int(num1)+int(num2)
+
+print("The sum of {} and {} is {}".format(num1,num2,sum))
